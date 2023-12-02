6b39de7b6ed616ba495ff3d96dfd3507510d2558
Stefan Nilsson <nilsson_stefan@yahoo.com>
2023-12-02 16:04:20 +0100

    Solution for dec 1

---
+++ day01.py
@@ -0,0 +1,71 @@
+import time
+import numpy as np
+import math
+import copy
+
+NUMBER_OF_CALLS = 0
+LOG_FILE = open('logfile.txt', 'w')
+DATA_CACHE = dict()
+USED = []
+
+
+def advent1_1():
+    file = open('input01.txt')
+    rows = list()
+    sum = 0
+    for line in file:
+        rows.append(line)
+        for c in line:
+            if c.isdigit():
+                first_dig = c
+                break
+        for c in reversed(line):
+            if c.isdigit():
+                last_dig = c
+                break
+
+        #print(first_dig, last_dig)
+        sum += int(first_dig + last_dig)
+    print('Sum : ', sum)
+
+
+def advent1_2():
+    file = open('input01.txt')
+    rows = list()
+    sum = 0
+    nums = {'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5', 'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
+    for line in file:
+        rows.append(line)
+        pos = 0
+        for c in line:
+            if c.isdigit():
+                first_dig = c
+                break
+            pos += 1
+        for n in nums.keys():
+            if line.find(n) < pos and line.find(n) != -1:
+                first_dig = nums[n]
+                pos = line.find(n)
+        pos = len(line) - 1
+        for c in reversed(line):
+            if c.isdigit():
+                last_dig = c
+                break
+            pos -= 1
+        for n in nums.keys():
+            if line.rfind(n) > pos and line.rfind(n) != -1:
+                last_dig = nums[n]
+                pos = line.rfind(n)
+
+        #print(first_dig, last_dig)
+        sum += int(first_dig + last_dig)
+    print('Sum : ', sum)
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+    print('Advent 1')
+    advent1_1()
+    advent1_2()
+    end_time_1 = time.time()
+    print("time elapsed: {:.2f}s".format(end_time_1 - start_time))
