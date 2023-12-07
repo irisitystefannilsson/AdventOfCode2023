e82f46af52e850f06c0b8c775b47b2e8920f142c
Stefan Nilsson <nilsson_stefan@yahoo.com>
2023-12-07 21:56:28 +0100

    day 6

---
+++ day06.py
@@ -0,0 +1,62 @@
+import time
+import numpy as np
+import math
+import copy
+
+NUMBER_OF_CALLS = 0
+LOG_FILE = open('logfile.txt', 'w')
+DATA_CACHE = dict()
+USED = []
+
+
+def tp_roots(d, T):
+    root1 = math.floor(T/2 + math.sqrt(T*T/4 - d))
+    root2 = math.ceil(T/2 - math.sqrt(T*T/4 - d))
+    if root1*(T - root1) <= d:
+        root1 -= 1
+    if root2*(T - root2) <= d:
+        root2 += 1
+    return root2, root1
+        
+def advent6_1():
+    #file = open('input06_example.txt')
+    file = open('input06.txt')
+    time_line = file.readline().strip('\n');
+    times = time_line.split(':')[1].split()
+    dist_line = file.readline().strip('\n');
+    dists = dist_line.split(':')[1].split()
+    #print(times)
+    #print(dists)
+    nofways = 1
+    for d, T in zip(dists, times):
+        r1, r2 = tp_roots(int(d), int(T))
+        print(r1, r2)
+        nofways *= (r2 - r1 + 1)
+
+    print('Nof ways:', nofways)
+    
+    
+def advent6_2():
+    #file = open('input06_example.txt')
+    file = open('input06.txt')
+    time_line = file.readline().strip('\n');
+    time = int(time_line.split(':')[1].replace(' ', ''))
+    dist_line = file.readline().strip('\n');
+    dist = int(dist_line.split(':')[1].replace(' ', ''))
+    print(time, dist)
+    r1, r2 = tp_roots(dist, time)
+    #print(r1, r2)
+    nofways = (r2 - r1 + 1)
+
+    print('Nof ways:', nofways)
+    
+
+    
+if __name__ == '__main__':
+
+    start_time = time.time()
+    print('Advent 6')
+    advent6_1()
+    advent6_2()
+    end_time_1 = time.time()
+    print("time elapsed: {:.2f}s".format(end_time_1 - start_time))
