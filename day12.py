03c0fb3e464f8d1341cd3dac0abf86fbe80ab1e5
Stefan Nilsson <nilsson_stefan@yahoo.com>
2023-12-16 20:27:16 +0100

    1/2 day 12  & 100% day 13

---
+++ day12.py
@@ -0,0 +1,93 @@
+import time
+import numpy as np
+import functools
+import math
+import copy
+
+NUMBER_OF_CALLS = 0
+LOG_FILE = open('logfile.txt', 'w')
+DATA_CACHE = dict()
+USED = []
+
+
+def ok_conf(conf : list, constr : list):
+    for l in range(min(len(conf), len(constr))):
+        if conf[l] == '#' and constr[l] not in ['#', '?']:
+            return False
+        if conf[l] == '.' and constr[l] not in ['.', '?']:
+            return False
+    return True
+
+
+def generate(numbers : list, constr : list, start=0, incoming=list()):
+    #print('Start->', start)
+    #print(incoming)
+    #print('Num:', numbers)
+    #print('Constr:', constr[start:])
+    lc = len(constr) - start
+    ln = sum(numbers) + len(numbers) - 1
+    nod = 0
+    if lc < ln:
+        return 0
+    for i in range(lc - ln + 1):
+        new_incoming = incoming + i*['.'] + numbers[0]*['#'] + ['.']
+        if ok_conf(new_incoming, constr):
+            if len(numbers) > 1:
+                nod += generate(numbers[1:], constr, start + i + numbers[0] + 1, new_incoming)
+            else:
+                new_incoming += (len(constr) - len(new_incoming))*['.']
+                if ok_conf(new_incoming, constr):
+                    nod += 1
+            
+    return nod
+
+    
+def advent12_1():
+    #file = open('input12_example.txt')
+    file = open('input12.txt')
+
+    sum_parr = 0
+    for line in file:
+        line = line.strip('\n')
+        springs, numbers = line.split(' ')
+        numbers = numbers.split(',')
+        numbers = [int(e) for e in numbers]
+        pbroke = springs.split('.')
+        pbroke = [e for e in pbroke if e != '']
+        parr = generate(numbers, springs)
+        #print('----------- parr : ', parr, '----------')
+        sum_parr += parr
+
+    print('Sum of arrs:' ,sum_parr)
+
+
+def advent12_2():
+    file = open('input12_example.txt')
+    #file = open('input12.txt')
+
+    sum_parr = 0
+    for line in file:
+        line = line.strip('\n')
+        springs, numbers = line.split(' ')
+        numbers = numbers.split(',')
+        numbers = [int(e) for e in numbers]
+        numbers = 5*numbers
+        springs = springs + '?' + springs + '?' + springs + '?' + springs + '?' + springs 
+        #print(springs, numbers)
+        pbroke = springs.split('.')
+        pbroke = [e for e in pbroke if e != '']
+        parr = generate(numbers, springs)
+        print('----------- parr : ', parr, '----------')
+        sum_parr += parr
+        #break
+    print('Sum of arrs:' ,sum_parr)
+    
+    
+if __name__ == '__main__':
+
+    start_time = time.time()
+    print('Advent 12')
+    advent12_1()
+    advent12_2()
+    end_time_1 = time.time()
+    print("time elapsed: {:.2f}s".format(end_time_1 - start_time))
---
+++ day13.py
@@ -0,0 +1,138 @@
+import time
+import numpy as np
+import functools
+import math
+import copy
+
+NUMBER_OF_CALLS = 0
+LOG_FILE = open('logfile.txt', 'w')
+DATA_CACHE = dict()
+USED = []
+
+
+def find_symmetry(pattern : list, noc = -1):
+    rows = len(pattern)
+    cols = len(pattern[0])
+    pat = np.full((rows, cols), '.', dtype=str)
+    for i in range(rows):
+        for j in range(cols):
+            pat[i, j] = pattern[i][j]
+
+    print(pat)
+    # check cols
+    for j in range(cols - 1):
+        sym = True
+        for jj in range(1, j + 2):
+            if j + jj >= cols:
+                continue
+            if (pat[:, j + 1 - jj] != pat[:, j + jj]).any():
+                sym = False
+                break
+        if sym:
+            if (noc >= 0 and DATA_CACHE[noc] != j + 1) or noc == -1:
+                return j + 1
+
+
+    # check rows
+    for i in range(rows - 1):
+        sym = True
+        for ii in range(1, i + 2):
+            if i + ii >= rows:
+                continue
+            if (pat[i + 1 - ii, :] != pat[i + ii]).any():
+                sym = False
+                break
+        if sym:
+            if (noc >= 0 and DATA_CACHE[noc] != (i + 1)*100) or noc == -1:
+                return (i + 1)*100
+
+    return 0
+
+    
+def advent13_1():
+    #file = open('input13_example.txt')
+    file = open('input13.txt')
+
+    sym_sum = 0
+    lines = list()
+    noc = 0
+    for line in file:
+        line = line.strip('\n')
+        if line == '':
+            sym = find_symmetry(lines)
+            DATA_CACHE[noc] = sym
+            print(DATA_CACHE)
+            noc += 1
+            sym_sum += sym
+            lines = list()
+        else:
+            lines.append(line)
+    # last pattern        
+    sym = find_symmetry(lines)
+    DATA_CACHE[noc] = sym
+    print(DATA_CACHE)
+    noc += 1
+    sym_sum += sym
+        
+    print('Sum of symms:' , sym_sum)
+
+
+def find_new_symmetry(pattern : list, noc : int):
+    rows = len(pattern)
+    cols = len(pattern[0])
+    for i in range(rows):
+        for j in range(cols):
+            if pattern[i][j] == '.':
+                pattern[i][j] = '#'
+                sym = find_symmetry(pattern, noc)
+                if sym > 0:
+                    return sym
+                else:
+                    pattern[i][j] = '.'
+            elif pattern[i][j] == '#':
+                pattern[i][j] = '.'
+                sym = find_symmetry(pattern, noc)
+                if sym > 0:
+                    return sym
+                else:
+                    pattern[i][j] = '#'
+            else:
+                print(pattern[i][j])
+                raise
+
+                    
+def advent13_2():
+    #file = open('input13_example.txt')
+    file = open('input13.txt')
+
+    sym_sum = 0
+    lines = list()
+    noc = 0
+    for line in file:
+        line = line.strip('\n')
+        if line == '':
+            print(DATA_CACHE)
+            print(noc)
+            sym = find_new_symmetry(lines, noc)
+            noc += 1
+            print(sym)
+            sym_sum += sym
+            lines = list()
+        else:
+            lines.append(list(line))
+    # last pattern        
+    sym = find_new_symmetry(lines, noc)
+    print(sym)
+    sym_sum += sym
+        
+    print('Sum of symms:' , sym_sum)
+    
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+    print('Advent 13')
+    advent13_1()
+    advent13_2()
+    end_time_1 = time.time()
+    print("time elapsed: {:.2f}s".format(end_time_1 - start_time))
